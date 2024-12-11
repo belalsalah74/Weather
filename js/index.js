c1c9c77c93fe1fc2606dce2c8258a759a12baeda
belalsalah <belalsalah74@gmailcom>
2024-12-11 22:59:21 +0200

    initial

---
+++ js/index.js
@@ -0,0 +1,179 @@
+const apiKey = "d839e7830c194ec798072324241012";
+const baseUrl = "https://api.weatherapi.com/v1";
+
+const elements = {
+  weatherContent: document.querySelector(".weather-content"),
+  errorMsg: document.getElementById("error-msg"),
+  spinnerHolder: document.querySelector(".spinner-holder"),
+  city: document.querySelector(".city"),
+  dayOne: document.querySelector("#day-1 .day"),
+  dayOneMonth: document.querySelector("#day-1 .day-month"),
+  dayOneDegree: document.querySelector("#day-1 .degree"),
+  dayOneIcon: document.querySelector("#day-1 .weather-icon"),
+  dayOneText: document.querySelector("#day-1 .text"),
+  dayTwo: document.querySelector("#day-2 .day"),
+  dayTwoMonth: document.querySelector("#day-2 .day-month"),
+  dayTwoDegree: document.querySelector("#day-2 .degree"),
+  dayTwoMinDegree: document.querySelector("#day-2 .min-degree"),
+  dayTwoIcon: document.querySelector("#day-2 .weather-icon"),
+  dayTwoText: document.querySelector("#day-2 .text"),
+  dayThree: document.querySelector("#day-3 .day"),
+  dayThreeMonth: document.querySelector("#day-3 .day-month"),
+  dayThreeDegree: document.querySelector("#day-3 .degree"),
+  dayThreeIcon: document.querySelector("#day-3 .weather-icon"),
+  dayThreeMinDegree: document.querySelector("#day-3 .min-degree"),
+  dayThreeText: document.querySelector("#day-3 .text"),
+  rain: document.querySelector(".rain"),
+  wind: document.querySelector(".wind"),
+  compass: document.querySelector(".compass"),
+  searchInput: document.getElementById("search"),
+  searchBtn: document.getElementById("search-btn"),
+};
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const compass = {
+  N: "North",
+  S: "South",
+  E: "East",
+  W: "West",
+  NE: "North East",
+  NW: "North West",
+  SE: "South East",
+  SW: "South West",
+  WNW: "West - North West",
+  NNW: "North - Nort hWest",
+  NNE: "Nort - North East",
+  ENE: "East - Borth East",
+  ESE: "East - South East",
+  SSE: "Sout - South East",
+  SSW: "Sout - South West",
+  WSW: "West - South West",
+};
+const forecasTdays = {
+  today: days[new Date().getDay()],
+  day2: days[new Date().getDay() + 1],
+  day3: days[new Date().getDay() + 2],
+  todayNumber: new Date().getDate(),
+  month: months[new Date().getMonth()],
+};
+
+
+init();
+
+
+function init() {
+  navigator.geolocation.getCurrentPosition(
+    (postion) => {
+      getData(`${postion.coords.latitude},${postion.coords.longitude}`);
+    },
+    (eror) => {
+      console.log(eror);
+      showError(
+        "Location permission denied please allow location or search for your city!"
+      );
+      elements.weatherContent.classList.add("d-none");
+    }
+  );
+}
+
+async function getData(location) {
+  elements.errorMsg.parentElement.classList.remove("d-none");
+  elements.spinnerHolder.classList.remove("d-none");
+  elements.weatherContent.classList.add("d-none");
+  try {
+    const request = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3`
+    );
+    response = await request.json();
+    if (response.error) {
+      showError(response.error.message);
+    }
+    displayData(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayData(response) {
+  if (response.error) {
+    elements.weatherContent.classList.add("d-none");
+  } else {
+    const date = new Date(response.forecast.forecastday[0].date);
+    elements.city.textContent = response.location.name;
+    elements.dayOne.textContent = forecasTdays.today;
+    elements.dayOneMonth.textContent = `${forecasTdays.todayNumber} ${forecasTdays.month}`;
+    elements.dayOneDegree.textContent = response.current.temp_c + "°c";
+    elements.dayOneIcon.src = response.current.condition.icon;
+    elements.dayOneText.textContent = response.current.condition.text;
+
+    elements.dayTwo.textContent = forecasTdays.day2;
+    elements.dayTwoMonth.textContent = `${date.getDate() + 1} ${
+      forecasTdays.month
+    }`;
+    elements.dayTwoDegree.textContent =
+      response.forecast.forecastday[1].day.maxtemp_c + "°c";
+    elements.dayTwoMinDegree.textContent =
+      response.forecast.forecastday[1].day.mintemp_c + "°c";
+    elements.dayTwoIcon.src =
+      response.forecast.forecastday[1].day.condition.icon;
+    elements.dayTwoText.textContent =
+      response.forecast.forecastday[1].day.condition.text;
+    null;
+    elements.dayThree.textContent = response.error ? forecasTdays.day3 : null;
+    elements.dayThreeMonth.textContent = `${date.getDate() + 2} ${
+      forecasTdays.month
+    }`;
+    elements.dayThreeDegree.textContent =
+      response.forecast.forecastday[2].day.maxtemp_c + "°c";
+    elements.dayThreeMinDegree.textContent =
+      response.forecast.forecastday[2].day.mintemp_c + "°c";
+    elements.dayThreeIcon.src =
+      response.forecast.forecastday[2].day.condition.icon;
+    elements.dayThreeText.textContent =
+      response.forecast.forecastday[2].day.condition.text;
+    elements.rain.append(`${response.current.humidity}%`);
+    elements.wind.append(`${response.current.wind_kph}km/h`);
+    elements.compass.append(compass[response.current.wind_dir]);
+    elements.errorMsg.parentElement.classList.add("d-none");
+    elements.weatherContent.classList.remove("d-none");
+    elements.spinnerHolder.classList.add("d-none");
+  }
+}
+function showError(msg) {
+  elements.spinnerHolder.classList.add("d-none");
+  elements.errorMsg.parentElement.classList.remove("d-none");
+  elements.errorMsg.textContent = msg;
+}
+
+elements.searchBtn.addEventListener("click", () => {
+  if (elements.searchInput.value) {
+    getData(elements.searchInput.value);
+  }
+});
+
+elements.searchInput.addEventListener("input", (e) => {
+  if (!e.target.value) {
+    init();
+  }
+});
